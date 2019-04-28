fa2d733af71ef53f1a75729a07fd3a37f8f43627
giparino <44357506+giparino@users.noreply.github.com>
2019-04-28 21:49:36 +0300

    change files

--- map/file.js
+++ map/file.js
@@ -3,55 +3,72 @@ let geocoder;
 let map;
 let service;
 let markers = [];
-let infos = [];
+let addrMarker;
+let icon;
 
 function initialize() {
 
     geocoder = new google.maps.Geocoder();
 
-    let center = new google.maps.LatLng(53.9021648, 27.5499173);
+    let center = new google.maps.LatLng(53.9021648, 27.5499173);    
     let myOptions = {
         zoom: 14,
         center: center
     };
     map = new google.maps.Map(document.getElementById('gmap_canvas'), myOptions);
 
-    document.getElementById("button1").addEventListener('click', findAddress);
-    document.getElementById("button2").addEventListener('click', findPlaces);
+    document.getElementById("button1").addEventListener('click', function () {
+        let address = document.getElementById("gmap_address").value;        
+        findAddress(address);
+    });
+    document.getElementById("button2").addEventListener('click', function () {
+        let address = map.getCenter();
+        findAddress(address);
+    });
+    document.getElementById("button3").addEventListener('click', findPlaces);
+    document.getElementById("button4").addEventListener('click', function () {
+        clearOverlays();
+        clearAddress();
+    });
+
+
 }
 
 
 // функция поиска адреса
-function findAddress() {
-    let address = document.getElementById("gmap_address").value;
+function findAddress(addr) {
+    let address = {};
+    if (typeof addr == 'object')
+        address = { 'latLng': addr };
+    else
+        address = { 'address': addr };
 
     // определение местоположения по адресу
-    geocoder.geocode({ 'address': address }, function (results, status) {
-        if (status == google.maps.GeocoderStatus.OK) {
-            console.log(results);
-            let newCenter = results[0].geometry.location;
-            console.log(newCenter);
+    geocoder.geocode(address, function (results, status) {
+        if (status == google.maps.GeocoderStatus.OK) {           
+            let newCenter = results[0].geometry.location;            
             map.setCenter(newCenter);
+           
 
             document.getElementById('lat').value = results[0].geometry.location.lat();
             document.getElementById('lng').value = results[0].geometry.location.lng();
 
-            var icon = {
-                url: 'marker.png',
-                scaledSize: new google.maps.Size(50, 50),
-                origin: new google.maps.Point(0, 0),
-                anchor: new google.maps.Point(0, 0)
+            let icon = {
+                url: 'https://theunrefined.com.au/wp-content/plugins/google-maps/assets/images/icons/pointcenter48.png',   //'marker.png',       //'http://maps.google.com/mapfiles/ms/icons/green-dot.png',    
+                scaledSize: new google.maps.Size(30, 30)               
             };
 
-            let addrMarker = new google.maps.Marker({
+            if (addrMarker)
+                addrMarker.setMap(null);
+
+            addrMarker = new google.maps.Marker({
                 position: newCenter,
                 map: map,
                 title: results[0].formatted_address,
                 icon: icon
+                //draggable:true
             });
-        } else {
-            alert('Ошибка: ' + status);
-        }
+        }       
     });
 }
 
@@ -71,14 +88,15 @@ function findPlaces() {
     };
 
     // отправляем запрос
-    service = new google.maps.places.PlacesService(map);
-    service.search(request, createMarkers);
+    service = new google.maps.places.PlacesService(map);   
+    service.nearbySearch(request, createMarkers);
+    service.nearbySearch(request, outputAddresses);    
 }
 
-// добавляем метки
+// добавляем все метки
 function createMarkers(results, status) {
     if (status == google.maps.places.PlacesServiceStatus.OK) {
-        clearOverlays();
+        clearOverlays();  // удаление маркеров с карты
 
         for (let i = 0; i < results.length; i++) {
             createMarker(results[i]);
@@ -87,31 +105,107 @@ function createMarkers(results, status) {
         alert('По вашему запросу ничего не найдено');
     }
 }
+let mark_prev_click;
+let a;
 
-// функция для добавления одной метки
+// добавление одной метки
 function createMarker(obj) {
-
-    console.log(obj);
-
+   
+    icon = {
+        url: 'https://theunrefined.com.au/wp-content/plugins/google-maps/assets/images/icons/marker128.png',       //'http://maps.google.com/mapfiles/ms/icons/green-dot.png',    
+        scaledSize: new google.maps.Size(30, 30)
+    };
     let mark = new google.maps.Marker({
         position: obj.geometry.location,
         map: map,
-        title: obj.name
+        title: obj.name,
+        opacity: 1,
+        icon: icon
     });
+
+
     markers.push(mark);
 
-    let infowindow = new google.maps.InfoWindow({
-        content: `<img class="img" src="` + obj.icon + '" />' + obj.name +
-            '<br />Рейтинг: ' + obj.rating +
-            '<br />Адрес: ' + obj.vicinity
+    let mark_mouseover = google.maps.event.addListener(mark, 'mouseover', function () {
+        let iconActive = {
+            url: 'https://theunrefined.com.au/wp-content/plugins/google-maps/assets/images/icons/greenmarker64.png',
+            scaledSize: new google.maps.Size(40, 40)
+        }
+        mark.setIcon(iconActive);        
     });
 
-    // обработчик клика по метке
-    google.maps.event.addListener(mark, 'click', function () {
-        clearInfos();
-        infowindow.open(map, mark);
+
+    let mark_mouseout = google.maps.event.addListener(mark, 'mouseout', function () {
+        mark.setIcon(icon);
     });
-    infos.push(infowindow);
+
+
+    // обработчик клика по метке
+    google.maps.event.addListener(mark, 'click', function (e) {       
+        if (mark_prev_click) {
+            mark_prev_click.setIcon(icon);
+            google.maps.event.addListener(mark_prev_click, 'mouseover', function () {
+                let iconActive = {
+                    url: 'https://theunrefined.com.au/wp-content/plugins/google-maps/assets/images/icons/greenmarker64.png',
+                    scaledSize: new google.maps.Size(40, 40)
+                }
+                mark.setIcon(iconActive);
+            });
+
+            google.maps.event.addListener(mark_prev_click, 'mouseout', function () {
+                mark.setIcon(icon);
+            });
+        }
+        mark.setIcon('https://theunrefined.com.au/wp-content/plugins/google-maps/assets/images/icons/redmarker48.png');
+        clearAddress();
+        outputAddress(obj);
+        mark_mouseout.remove();
+        mark_mouseover.remove();
+        mark_prev_click = mark;
+    });
+
+
+}
+
+//вывод всех адресов
+function outputAddresses(results, status) {
+    if (status == google.maps.GeocoderStatus.OK) {        
+        for (let i = 0; i < results.length; i++) {
+            outputAddress(results[i]);
+        }
+    }
+}
+
+
+// вывод одного адреса
+function outputAddress(obj) {
+
+    let request = {
+        placeId: obj.place_id
+    }
+    service.getDetails(request, function (place, status) {
+        if (status === window.google.maps.places.PlacesServiceStatus.OK) {
+                      
+
+            let info = document.createElement('div');
+            info.classList.add('info');
+            info.innerHTML = `<div class="wrapper_for_img"><div class="address-img" style="background-image:url('` +
+                place.photos[0].getUrl({ maxWidth: 180, maxHeight: 180 }) + `')"></div></div>` +
+                '<p class="address-name">' + obj.name + '</p>' +
+                '<p class="address-rating">Рейтинг: ' + obj.rating + '</p>' +
+                '<p class="address-address">' + obj.vicinity + '</p>' +
+                '<p class="address-phone">' + place.formatted_phone_number + '</p>' +
+                '<p class="address-opening-hours"><span><b>Время работы: </b></span>' + place.opening_hours.weekday_text[new Date().getDay()].slice(-11) + '</p>';
+
+            search_result_address.appendChild(info);
+        }
+    });
+}
+
+
+//удаление выведенных адресов
+function clearAddress() {
+    search_result_address.innerHTML = '';
 }
 
 //удаление маркеров с карты
@@ -121,18 +215,15 @@ function clearOverlays() {
             markers[i].setMap(null);
         }
         markers = [];
-        infos = [];
     }
 }
 
-//закрытие окон с информацией
-function clearInfos() {
-    if (infos) {
-        for (i in infos) {
-            if (infos[i].getMap()) {
-                infos[i].close();
-            }
+function clearClickMarker() {
+    if (markers) {
+        for (i in markers) {
+            markers[i].setIcon(icon);
         }
     }
 }
+
 
